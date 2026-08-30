3179f25b2c08923b8d0a575ec2d7cd1ea5eeec22
Arun Prakash Jana <engineerarun@gmail.com>
2026-08-30 21:13:47 +0530

    Add preferred default tools on Xubuntu

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
-const char* chndlr_fallback_cmd = "firefox ";
+const char* chndlr_fallback_cmd = "xdg-open ";
 
 #define P(RE,...) { RE, (const char*[]) { __VA_ARGS__, NULL} }
 
@@ -9,19 +9,34 @@ const char* chndlr_fallback_cmd = "firefox ";
 static const Pair pairs[] = {
 	/* regex                  action */
 
-	/* files */
-	P( "\\.(jpg|png|tiff|gif)$","nsxiv", "%s"        ),
-	P( "\\.mp3$",               "st", "-e", "mplayer", "%s"),
+	/* image files */
+	P( "\\.(jpe?g|png|gif|webp|tiff|bmp|ico|svg)$", "gpicview", "%s" ),
+
+	/* audio: copy the nnn plugin mocq to /usr/local/bin/ */
+	P( "\\.(aac|flac|m4a|mid|midi|mpa|mp2|mp3|ogg|wav|wma)$",
+		"sh", "-c", "if command -v mocq >/dev/null 2>&1; then exec mocq \"$1\" opener; elif command -v mocp >/dev/null 2>&1; then exec mocp -a \"$1\"; else exec mpv \"$1\"; fi", "audio-open", "%s" ),
+
+	/* video */
+	P( "\\.(avi|mkv|mp4|mov|webm|m4v)$", "mpv", "%s" ),
+
+	/* pdf */
+	P( "\\.(pdf)$", "zathura", "%s" ),
+
+	/* Office document (WPS Office) */
+	P( "\\.(doc|docx|rtf)$", "wps", "%s" ),
+	P( "\\.(xls|xlsx|csv)$", "et", "%s" ),
+	P( "\\.(ppt|pptx)$", "wpp", "%s" ),
+
+	/* text (ignored - use nnn options -e and -E to let nnn open text files in the terminal) */
+	P( "\\.(txt|md|rst)$", "mousepad", "%s" ),
+	P( "\\.(htm|html|xhtml)$", "xdg-open", "%s" ),
 
 	/* web */
+	// P( "^(mailto:|https?://|ftp://)", "xdg-open", "%s" ),
 
 	/* youtube */
-	P( WEB_PREFIX("youtube.com/watch\\?|youtu\\.be/"), "mpv", "%s"),
-
-	/* github */
-	P( WEB_PREFIX("github.com") "/([^/]+)/([^/]+)/actions",
-								"gh", "run", "list", "--repo", "%1/%2/%3" ),
-	P( WEB_PREFIX("github.com"), "lynx", "%s" ),
+	// P( WEB_PREFIX("youtube.com/watch\\?|youtu\\.be/"), "mpv", "%s"),
+	// P( WEB_PREFIX("github.com"), "xdg-open", "%s" ),
 };
 
 #undef P
